bdb5e87e3bea8a2897722da11689f99427a90a43
jesse cole <cole.j.jesse@gmail.com>
2022-10-15 00:46:22 -0700

    friday push

---
+++ Web Fund/Finding_Connections/script.js
@@ -0,0 +1,9 @@
+console.log("page loaded...");
+var friendRequest = document.querySelector("card-list-item")
+
+function nameChange() {
+    myName.innerText = "Jesse Cole";
+}
+function friendAction() {
+    card-list-item.remove();
+}
---
+++ Web Fund/practice/script.js
@@ -0,0 +1,10 @@
+var kasey = document.querySelector("#thePic")
+
+
+function pickDog(element) {
+    kasey.src = "images/dog.webp"
+    element.style.backgroundColor = "red"
+}
+function pickCat(element) {
+    kasey.src = "images/cat.webp"
+}
