97e32ff6b8143d1dee5fe6bc1c1d38fc92d599e6
jesse cole <cole.j.jesse@gmail.com>
2022-10-20 00:03:29 -0700

    loops challenge

--- Web Fund/DojoWeather/script.js
+++ Web Fund/DojoWeather/script.js
@@ -4,8 +4,8 @@
 //     document.querySelector("#choicethree")
 // )
 // var cities = ["Burbank", "Chicago", "Dallas"]
-var cookie = document.querySelector(".cookie")
-var leftDegrees = document.querySelector(".leftdegree")
+// var cookie = document.querySelector(".cookie")
+// var leftDegrees = document.querySelector(".leftdegree")
 var left = [75, 80, 69, 78]
 var right = [65, 66, 61, 70]
 
@@ -17,6 +17,31 @@ function acceptCookie() {
     cookie.remove();
 }
 
-function tempChange() {
-    leftDegrees.innerText = ;
+
+
+function tempChange(element) {
+    for(var i = 1; i<9; i++) {
+        var degreeElement = document.querySelector(".degree" + i)
+        var degree = parseInt(degreeElement.innerText)
+        if(element.value == "celcius") {
+            degreeElement.innerText = fromFarenheit(degree) + "°"
+        }
+        else {
+            degreeElement.innerText = fromCelcius(degree) + "°"
+        }
+    }
 }
+
+
+
+function fromCelcius(temp) {
+    var x = Math.round(9 / 5 * temp + 32)
+    return x
+}
+console.log(fromCelcius(57))
+
+function fromFarenheit(temp) {
+    var y = Math.round(5 / 9 * (temp - 32))
+    return y
+}
+console.log(fromFarenheit(78))
