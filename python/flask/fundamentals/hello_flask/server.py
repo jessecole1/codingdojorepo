f3b450cddccdc3d9eac630b605ccbafc8604974d
jesse cole <cole.j.jesse@gmail.com>
2022-11-16 01:43:02 -0800

    html table

--- python/flask/fundamentals/hello_flask/server.py
+++ python/flask/fundamentals/hello_flask/server.py
@@ -1,11 +1,24 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+       {'name' : 'Michael', 'age' : 35},
+       {'name' : 'John', 'age' : 30 },
+       {'name' : 'Mark', 'age' : 25},
+       {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
 @app.route('/hello/<name>')
 def success(name):
     return(f'Hello {name.upper()}')
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+    app.run(host='0.0.0.0', port=5001)    # Run the app in debug mode.
