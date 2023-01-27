980ee76993157b9ad6d2db119967ed75a6ac6da1
jesse cole <cole.j.jesse@gmail.com>
2023-01-26 23:23:37 -0800

    new commit

---
+++ Java/SpringProjects/controllersPractice/src/test/java/com/codingdojo/controllersPractice/ControllersPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.controllersPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllersPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
