4dcf4b6ac4abd428bd24cc19f6e0089d3fd02d87
jesse cole <cole.j.jesse@gmail.com>
2023-01-29 16:01:29 -0800

    new helloWorld

---
+++ Java/SpringProjects/jstlPractice/src/main/java/com/codingdojo/jstlpractice/JstlPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.jstlpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstlPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstlPracticeApplication.class, args);
+	}
+
+}
---
+++ Java/SpringProjects/jstlPractice/src/test/java/com/codingdojo/jstlpractice/JstlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.jstlpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JstlPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
