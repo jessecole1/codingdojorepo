b8118f310068796efc084d978f446db3bb403932
jesse cole <cole.j.jesse@gmail.com>
2023-02-05 18:22:22 -0800

    book project practice

---
+++ Java/SpringProjects/mvc/src/main/java/com/codingdojo/mvc/repositories/BookRepository.java
@@ -0,0 +1,24 @@
+package com.codingdojo.mvc.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codingdojo.mvc.models.Book;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long>{
+	// retrieves all the books from the database
+	List<Book> findAll();
+	
+	// finds books with the description containing the search string
+	List<Book> findByDescriptionContaining(String search);
+	
+	// counts how many titles contains a certain string
+	Long countByTitleContaining(String search);
+	
+	// deletes a book that starts with a specific title
+	Long deleteByTitleStartingWith(String search);
+
+}
