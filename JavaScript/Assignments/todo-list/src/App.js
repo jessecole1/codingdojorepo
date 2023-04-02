7b16f31814291bb6e36b4e647932eee473dcb745
jesse cole <cole.j.jesse@gmail.com>
2023-04-01 18:25:26 -0700

    todo list project javascript

--- JavaScript/Assignments/todo-list/src/App.js
+++ JavaScript/Assignments/todo-list/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Task from "./components/Task";
+import Todo from "./components/Todo";
 import React, {useState} from 'react';
 
 function App() {
@@ -10,9 +10,7 @@ function App() {
 
   const handleNewTodo = (e) => {
     e.preventDefault();
-    if (newTodo.length === 0) {
-      return;
-    }
+    if (newTodo.length === 0) {return}
 
     const todoItem = {
       text: newTodo,
@@ -24,32 +22,38 @@ function App() {
   }
 
   const handleDelete = (deleteIdx) => {
-    const filteredIdx = todoList.filter((todo, idx) => {
-      return idx !== deleteIdx;
-    })
+    const filteredIdx = todoList.filter((todo, idx) => {return idx !== deleteIdx;})
     setTodoList(filteredIdx)
   }
 
+  const handleToggleComplete = (idx) => {
+    const updatedTodos = todoList.map((todo, i) => {
+      if (idx === i) {todo.complete = !todo.complete}
+      return todo;
+    })
+    setTodoList(updatedTodos);
+  }
+
+
   return (
     <div className="App">
-      <form onSubmit={(e) => {
-        handleNewTodo(e);
-      }}>
-        <input onChange={(e) => {
-          setNewTodo(e.target.value);
-        }} type="text" value={newTodo}/>
+      <form onSubmit={(e) => {handleNewTodo(e);}}>
+        <input onChange={(e) => {setNewTodo(e.target.value)}} type="text" value={newTodo}/>
         <div>
           <button>Add</button>
         </div>
       </form>
         {
           todoList.map((item, idx) => {
+
+            const classArr = [];
+            if (item.complete) {classArr.push("lineThrough")}
+
             return (
             <div key={idx}>
-              <span>{item.text}</span>
-              <span><button style={{marginLeft: "20px"}} onClick={(e) => {
-                handleDelete(idx)
-              }}>Delete</button></span>
+              <input onChange={(event) => {handleToggleComplete(idx)}} type="checkbox"/>
+              <span className={classArr.join(" ")}>{item.text}</span>
+              <span><button style={{marginLeft: "20px"}} onClick={(e) => {handleDelete(idx)}}>Delete</button></span>
             </div>
             )
           })
---
+++ JavaScript/Assignments/todo-list/src/components/Todo.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const Todo = (props) => {
+
+    const {todoList} = props
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Todo;
